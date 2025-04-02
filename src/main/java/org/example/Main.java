4439c30f21796a12dc7a2314d91323fceeeb8160
ToniR90 <romy90.t@gmail.com>
2025-04-02 16:49:55 +0200

    Update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Person arg1 = new Person("Toni" , "Romero" , 35);
+        String arg2 = "This is a String";
+        int arg3 = 2025;
+
+        GenericMethods object1 = new GenericMethods<>();
+        object1.printGenericArgs(arg1 , arg2 , arg3);
     }
 }
